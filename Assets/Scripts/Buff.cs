217d0493c58c74a2f6a96e5aa58d29f44081fb0e
Eduardo Acosta <lalo_ar03@hotmail.com>
2023-11-07 21:46:42 -0700

    Add Buff script smooth transform movement

---
+++ Assets/Scripts/Buff.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff : MonoBehaviour
+{
+    private float startPosX;
+    private float startPosY;
+    public float posOffsetX = 1.43f;
+    public float posOffsetY = 3.11f;
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    
+    void Update()
+    {
+        startPosX = player.transform.position.x - posOffsetX;
+        startPosY = player.transform.position.y + posOffsetY;
+
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, startPosX, 0.1f),
+                                         Mathf.Lerp(transform.position.y, startPosY, 0.1f),
+                                         transform.position.z);
+    }
+}
